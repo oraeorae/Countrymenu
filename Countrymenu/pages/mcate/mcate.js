d616ae843addd54cd40a23e6a0b959e5c4ecd817
michael <75024156+TSSHA@users.noreply.github.com>
2022-05-16 17:54:08 +0800

    mcate(page) update

--- Countrymenu/pages/mcate/mcate.js
+++ Countrymenu/pages/mcate/mcate.js
@@ -1,10 +1,17 @@
 // pages/marketcategory/marketcategory.js
+import {firstData,secondData,thirdData} from "./mcatedata.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    firstData, //初始数据
+    secondData, //初始数据
+    thirdData, //初始数据
+    catesData:[],
+    catesData2:[],
+    goodsData: []
 
   },
 
@@ -12,14 +19,22 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let newArray1 = this.data.firstData;
+    let newArray2 = this.data.secondData;
+    let newArray3 = this.data.thirdData;
+    this.setData({
+      catesData:newArray1,
+      catesData2:newArray2,
+      goodsData:newArray3
+    })
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
---
+++ Countrymenu/pages/mcate/mcatedata.js
@@ -0,0 +1,126 @@
+// 静态模拟数据
+const firstData = [
+  {
+    "cat_id": 1,
+    "cat_name": "水果鲜花",
+    "cat_level": 0,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/13/1bcee70b-86d0-4e8d-9f16-930208c6113d.png",
+  },
+  {
+    "cat_id": 1,
+    "cat_name": "水果鲜花",
+    "cat_level": 0,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/13/1bcee70b-86d0-4e8d-9f16-930208c6113d.png",
+  },
+  {
+    "cat_id": 1,
+    "cat_name": "水果鲜花",
+    "cat_level": 0,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/13/1bcee70b-86d0-4e8d-9f16-930208c6113d.png",
+  },
+  {
+    "cat_id": 1,
+    "cat_name": "水果鲜花",
+    "cat_level": 0,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/13/1bcee70b-86d0-4e8d-9f16-930208c6113d.png",
+  },
+  {
+    "cat_id": 1,
+    "cat_name": "水果鲜花",
+    "cat_level": 0,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/13/1bcee70b-86d0-4e8d-9f16-930208c6113d.png",
+  },
+  {
+    "cat_id": 1,
+    "cat_name": "水果鲜花",
+    "cat_level": 0,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/13/1bcee70b-86d0-4e8d-9f16-930208c6113d.png",
+  },
+  {
+    "cat_id": 1,
+    "cat_name": "水果鲜花",
+    "cat_level": 0,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/13/1bcee70b-86d0-4e8d-9f16-930208c6113d.png",
+  },
+  {
+    "cat_id": 1,
+    "cat_name": "水果鲜花",
+    "cat_level": 0,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/13/1bcee70b-86d0-4e8d-9f16-930208c6113d.png",
+  }
+  
+
+  
+]
+const secondData = [
+  {
+    "cat_id": 3,
+    "cat_name": "新鲜水果",
+    "cat_level": 1,
+  },
+  {
+    "cat_id": 4,
+    "cat_name": "荔枝/热带果",
+    "cat_level": 1,
+  },
+  {
+    "cat_id": 5,
+    "cat_name": "葡提浆果",
+    "cat_level": 1,
+  },
+  {
+    "cat_id": 6,
+    "cat_name": "蜜瓜/西瓜",
+    "cat_level": 1,
+  },
+  {
+    "cat_id": 7,
+    "cat_name": "苹果香蕉梨",
+    "cat_level": 1,
+  },
+  {
+    "cat_id": 8,
+    "cat_name": "柑橘橙柚",
+    "cat_level": 1,    
+}
+]
+
+const thirdData = [
+  {
+    "cat_id": 5,
+    "goods_name": "妃子笑荔枝 250g",
+    "goods_describe":"甜蜜多汁|核小肉厚",
+    "goods_price":"￥13.89",
+    "cat_level": 2,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/11/9f90fa9a-302f-4340-944c-e85a0a7a0b08.jpg"
+  },
+  {
+    "cat_id": 6,
+    "goods_name": "春雪蜜桃 300g",
+    "goods_describe":"入口清脆|清新桃味入口清脆|清新桃味入口清脆|清新桃味",
+    "goods_price":"￥6.9",
+    "cat_level": 2,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/11/71c726ce-4a5f-48b4-87d7-8e363eb11ca9.jpg"
+  }
+
+
+
+
+]
+
+ 
+export {
+  firstData,
+  secondData,
+  thirdData
+}
