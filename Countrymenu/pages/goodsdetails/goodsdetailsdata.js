aad5d4ab5b52b163c2c480032e482d7921eb2011
michael <75024156+TSSHA@users.noreply.github.com>
2022-05-17 18:56:20 +0800

    goodsdetails页面

---
+++ Countrymenu/pages/goodsdetails/goodsdetailsdata.js
@@ -0,0 +1,37 @@
+
+
+const forthData = 
+  {
+    "cat_id": 1,
+    "cat_name": "妃子笑荔枝 250g",
+    "cat_level": 0,
+    "culture_desc":"甜蜜多汁|核小肉厚",
+    "price":8.99,
+    "origin":"广东省菜篮子工程惠东中源蔬菜基地",
+    "effect":"好吃就完事了",
+    "icon": [
+      {
+        "cat_icon": "https://dcdn.it120.cc/2022/05/11/9f90fa9a-302f-4340-944c-e85a0a7a0b08.jpg",
+       "cat_level": 2
+      },
+      {
+        "cat_icon": "https://dcdn.it120.cc/2022/05/11/9f90fa9a-302f-4340-944c-e85a0a7a0b08.jpg",
+       "cat_level": 2
+      },
+      {
+        "cat_icon": "https://dcdn.it120.cc/2022/05/11/9f90fa9a-302f-4340-944c-e85a0a7a0b08.jpg",
+       "cat_level": 2
+      },
+      {
+        "cat_icon": "https://dcdn.it120.cc/2022/05/11/9f90fa9a-302f-4340-944c-e85a0a7a0b08.jpg",
+       "cat_level": 2
+      },
+   ],
+   
+  }
+
+
+
+export {
+  forthData
+}
