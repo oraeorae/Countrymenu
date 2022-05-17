fd6e3572c2db69b4ab613a21e65f9b2e58f7fa4c
michael <75024156+TSSHA@users.noreply.github.com>
2022-05-17 17:59:26 +0800

    menudetails页面

---
+++ Countrymenu/pages/menudetails/menudetails.js
@@ -0,0 +1,71 @@
+// pages/menudetails/menudetails.js
+import {forthData} from "./menudetailsdata"
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    forthData,
+    menuData: [],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let newArray4 = this.data.forthData;
+    this.setData({
+      menuData:newArray4
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ Countrymenu/pages/menudetails/menudetailsdata.js
@@ -0,0 +1,77 @@
+
+
+const forthData = 
+  {
+    "cat_id": 1,
+    "cat_name": "素菜卷饼",
+    "cat_level": 0,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/17/86432326-a6f2-4de9-926b-df9c0c9768c4.jpg",
+    "culture_desc":"文化特色文化特色文化特色文化特色文化特色文化特色文化特色文化特色文化特色文化特色",
+    "cailiao": [
+      {
+       "cailiao_name": "水：100g",
+       "cat_level": 2
+     },
+     {
+       "cailiao_name": "辣椒丝：500g",
+       "cat_level": 2
+     },
+     {
+       "cailiao_name": "酱油：20g",
+       "cat_level": 2
+     },
+     {
+       "cailiao_name": "干辣椒：100g",
+       "cat_level": 2
+     },
+     {
+       "cailiao_name": "五香粉：3g",
+       "cat_level": 2
+     },
+     {
+       "cailiao_name": "土豆丝：800g",
+       "cat_level": 2
+     },
+     {
+       "cailiao_name": "食用油：200g",
+       "cat_level": 2
+     },
+     {
+       "cailiao_name": "葱：400g",
+       "cat_level": 2
+     }
+   ],
+   "buzhou": [
+    {
+     "descibe": "鸡蛋打散加入面粉加入水一起搅拌",
+     "cat_icon": "https://dcdn.it120.cc/2022/05/17/86432326-a6f2-4de9-926b-df9c0c9768c4.jpg",
+     "cat_level": 2
+   },
+   {
+    "descibe": "搅拌均匀适当再加入少许水",
+    "cat_icon": "https://dcdn.it120.cc/2022/05/17/86432326-a6f2-4de9-926b-df9c0c9768c4.jpg",
+    "cat_level": 2
+   },
+   {
+    "descibe": "成糊状",
+    "cat_icon": "https://dcdn.it120.cc/2022/05/17/86432326-a6f2-4de9-926b-df9c0c9768c4.jpg",
+    "cat_level": 2
+   },
+   {
+    "descibe": "准备开胃配菜酸萝卜丁，可不放",
+    "cat_icon": "https://dcdn.it120.cc/2022/05/17/86432326-a6f2-4de9-926b-df9c0c9768c4.jpg",
+    "cat_level": 2
+   },
+   {
+    "descibe": "干辣椒切碎",
+    "cat_icon": "https://dcdn.it120.cc/2022/05/17/86432326-a6f2-4de9-926b-df9c0c9768c4.jpg",
+    "cat_level": 2
+   }
+ ],
+  }
+
+
+
+export {
+  forthData
+}
