7988660866f013ef9966c0d9917a8258caef0fa5
michael <75024156+TSSHA@users.noreply.github.com>
2022-05-17 14:04:00 +0800

    vant框架导入+mcates页面

---
+++ Countrymenu/pages/mcates/mcatesdata.js
@@ -0,0 +1,228 @@
+
+
+const forthData = [
+  {
+    "cat_id": 1,
+    "cat_name": "蔬菜豆制品",
+    "cat_level": 0,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/13/6ef9a9fd-a18e-422c-9526-b6e5adcbc890.png",
+    "children": [
+      {
+       "cat_id": 5,
+       "goods_name": "妃子笑荔枝1 250g",
+       "goods_describe":"甜蜜多汁|核小肉厚",
+       "goods_price":"13.89",
+       "cat_level": 2,
+       "cat_deleted": false,
+       "cat_icon": "https://dcdn.it120.cc/2022/05/11/9f90fa9a-302f-4340-944c-e85a0a7a0b08.jpg"
+     },
+     {
+       "cat_id": 6,
+       "goods_name": "春雪蜜桃1 300g",
+       "goods_describe":"入口清脆|清新桃味入口清脆|清新桃味入口清脆|清新桃味",
+       "goods_price":"6.9",
+       "cat_level": 2,
+       "cat_deleted": false,
+       "cat_icon": "https://dcdn.it120.cc/2022/05/11/71c726ce-4a5f-48b4-87d7-8e363eb11ca9.jpg"
+     }
+   ]
+  },
+  {
+    "cat_id": 2,
+    "cat_name": "肉禽蛋",
+    "cat_level": 0,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/13/b31d209d-ebb3-4d29-ada2-160cd4bd7259.png",
+    "children": [
+      {
+       "cat_id": 5,
+       "goods_name": "妃子笑荔枝2 250g",
+       "goods_describe":"甜蜜多汁|核小肉厚",
+       "goods_price":"13.89",
+       "cat_level": 2,
+       "cat_deleted": false,
+       "cat_icon": "https://dcdn.it120.cc/2022/05/11/9f90fa9a-302f-4340-944c-e85a0a7a0b08.jpg"
+     },
+     {
+       "cat_id": 6,
+       "goods_name": "春雪蜜桃2 300g",
+       "goods_describe":"入口清脆|清新桃味入口清脆|清新桃味入口清脆|清新桃味",
+       "goods_price":"6.9",
+       "cat_level": 2,
+       "cat_deleted": false,
+       "cat_icon": "https://dcdn.it120.cc/2022/05/11/71c726ce-4a5f-48b4-87d7-8e363eb11ca9.jpg"
+     }
+   ]
+  },
+  {
+    "cat_id": 3,
+    "cat_name": "海鲜水产",
+    "cat_level": 0,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/13/e76c6a41-6a0e-48a1-ab1a-668f4e7fdcc0.png",
+    "children": [
+      {
+       "cat_id": 5,
+       "goods_name": "妃子笑荔枝3 250g",
+       "goods_describe":"甜蜜多汁|核小肉厚",
+       "goods_price":"13.89",
+       "cat_level": 2,
+       "cat_deleted": false,
+       "cat_icon": "https://dcdn.it120.cc/2022/05/11/9f90fa9a-302f-4340-944c-e85a0a7a0b08.jpg"
+     },
+     {
+       "cat_id": 6,
+       "goods_name": "春雪蜜桃3 300g",
+       "goods_describe":"入口清脆|清新桃味入口清脆|清新桃味入口清脆|清新桃味",
+       "goods_price":"6.9",
+       "cat_level": 2,
+       "cat_deleted": false,
+       "cat_icon": "https://dcdn.it120.cc/2022/05/11/71c726ce-4a5f-48b4-87d7-8e363eb11ca9.jpg"
+     }
+   ]
+  },
+  {
+    "cat_id": 4,
+    "cat_name": "水果鲜花",
+    "cat_level": 0,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/13/1bcee70b-86d0-4e8d-9f16-930208c6113d.png",
+    "children": [
+      {
+       "cat_id": 5,
+       "goods_name": "妃子笑荔枝4 250g",
+       "goods_describe":"甜蜜多汁|核小肉厚",
+       "goods_price":"13.89",
+       "cat_level": 2,
+       "cat_deleted": false,
+       "cat_icon": "https://dcdn.it120.cc/2022/05/11/9f90fa9a-302f-4340-944c-e85a0a7a0b08.jpg"
+     },
+     {
+       "cat_id": 6,
+       "goods_name": "春雪蜜桃4 300g",
+       "goods_describe":"入口清脆|清新桃味入口清脆|清新桃味入口清脆|清新桃味",
+       "goods_price":"6.9",
+       "cat_level": 2,
+       "cat_deleted": false,
+       "cat_icon": "https://dcdn.it120.cc/2022/05/11/71c726ce-4a5f-48b4-87d7-8e363eb11ca9.jpg"
+     }
+   ]
+  },
+  {
+    "cat_id": 5,
+    "cat_name": "乳品",
+    "cat_level": 0,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/13/0f1f5bea-6774-4529-9005-36f932f28462.png",
+    "children": [
+      {
+       "cat_id": 5,
+       "goods_name": "妃子笑荔枝5 250g",
+       "goods_describe":"甜蜜多汁|核小肉厚",
+       "goods_price":"13.89",
+       "cat_level": 2,
+       "cat_deleted": false,
+       "cat_icon": "https://dcdn.it120.cc/2022/05/11/9f90fa9a-302f-4340-944c-e85a0a7a0b08.jpg"
+     },
+     {
+       "cat_id": 6,
+       "goods_name": "春雪蜜桃5 300g",
+       "goods_describe":"入口清脆|清新桃味入口清脆|清新桃味入口清脆|清新桃味",
+       "goods_price":"6.9",
+       "cat_level": 2,
+       "cat_deleted": false,
+       "cat_icon": "https://dcdn.it120.cc/2022/05/11/71c726ce-4a5f-48b4-87d7-8e363eb11ca9.jpg"
+     }
+   ]
+  },
+  {
+    "cat_id": 6,
+    "cat_name": "粮油调味",
+    "cat_level": 0,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/13/d1386554-5453-4a17-8b5c-9f98dabb82d5.png",
+    "children": [
+      {
+       "cat_id": 5,
+       "goods_name": "妃子笑荔枝6 250g",
+       "goods_describe":"甜蜜多汁|核小肉厚",
+       "goods_price":"13.89",
+       "cat_level": 2,
+       "cat_deleted": false,
+       "cat_icon": "https://dcdn.it120.cc/2022/05/11/9f90fa9a-302f-4340-944c-e85a0a7a0b08.jpg"
+     },
+     {
+       "cat_id": 6,
+       "goods_name": "春雪蜜桃6 300g",
+       "goods_describe":"入口清脆|清新桃味入口清脆|清新桃味入口清脆|清新桃味",
+       "goods_price":"6.9",
+       "cat_level": 2,
+       "cat_deleted": false,
+       "cat_icon": "https://dcdn.it120.cc/2022/05/11/71c726ce-4a5f-48b4-87d7-8e363eb11ca9.jpg"
+     }
+   ]
+  },
+  {
+    "cat_id": 7,
+    "cat_name": "酒水饮料",
+    "cat_level": 0,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/13/5f183e59-6548-45be-8660-a0d3758d822a.png",
+    "children": [
+      {
+       "cat_id": 5,
+       "goods_name": "妃子笑荔枝7 250g",
+       "goods_describe":"甜蜜多汁|核小肉厚",
+       "goods_price":"13.89",
+       "cat_level": 2,
+       "cat_deleted": false,
+       "cat_icon": "https://dcdn.it120.cc/2022/05/11/9f90fa9a-302f-4340-944c-e85a0a7a0b08.jpg"
+     },
+     {
+       "cat_id": 6,
+       "goods_name": "春雪蜜桃7 300g",
+       "goods_describe":"入口清脆|清新桃味入口清脆|清新桃味入口清脆|清新桃味",
+       "goods_price":"6.9",
+       "cat_level": 2,
+       "cat_deleted": false,
+       "cat_icon": "https://dcdn.it120.cc/2022/05/11/71c726ce-4a5f-48b4-87d7-8e363eb11ca9.jpg"
+     }
+   ]
+  }
+  ,{
+    "cat_id": 8,
+    "cat_name": "熟食预制菜",
+    "cat_level": 0,
+    "cat_deleted": false,
+    "cat_icon": "https://dcdn.it120.cc/2022/05/13/982aeee6-6a68-4408-8c54-65d0e52fda10.png",
+    "children": [
+      {
+       "cat_id": 5,
+       "goods_name": "妃子笑荔枝8 250g",
+       "goods_describe":"甜蜜多汁|核小肉厚",
+       "goods_price":"13.89",
+       "cat_level": 2,
+       "cat_deleted": false,
+       "cat_icon": "https://dcdn.it120.cc/2022/05/11/9f90fa9a-302f-4340-944c-e85a0a7a0b08.jpg"
+     },
+     {
+       "cat_id": 6,
+       "goods_name": "春雪蜜桃8 300g",
+       "goods_describe":"入口清脆|清新桃味入口清脆|清新桃味入口清脆|清新桃味",
+       "goods_price":"6.9",
+       "cat_level": 2,
+       "cat_deleted": false,
+       "cat_icon": "https://dcdn.it120.cc/2022/05/11/71c726ce-4a5f-48b4-87d7-8e363eb11ca9.jpg"
+     }
+   ]
+  }
+
+
+
+
+]
+
+export {
+  forthData
+}
